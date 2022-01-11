f59bd1b01d13189de9790993eb683bcb0a3fc8b8
Luka-dot <79lukask@gmail.com>
2022-01-11 10:56:44 -0500

    added textInputs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -30,6 +31,9 @@ class MyHomePage extends StatelessWidget {
     )
   ];
 
+  String titleInput;
+  String amountInput;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,16 +41,51 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Card(
-            color: Colors.blue,
-            child: Container(
-              child: Text('CHART placeholder'),
-              width: double.infinity, // double.infinity
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Container(
+                child: Text('CHART placeholder'),
+                width: double.infinity, // double.infinity
+              ),
+              elevation: 5.0,
             ),
-            elevation: 5.0,
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (value) {
+                      titleInput = value;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    onChanged: (value) {
+                      amountInput = value;
+                    },
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.purple,
+                    ),
+                    onPressed: () {
+                      print(titleInput);
+                      print(amountInput);
+                    },
+                    child: Text('Add Transaction'),
+                  ),
+                ],
+              ),
+            ),
           ),
           Column(
             children: transactions.map((tx) {
@@ -78,7 +117,7 @@ class MyHomePage extends StatelessWidget {
                               fontFamily: 'roboto'),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Colors.blueGrey),
