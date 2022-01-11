078a4fb1ae882787e91c483cce30f3477a089d1d
Luka-dot <79lukask@gmail.com>
2022-01-11 07:46:27 -0500

    small UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -54,20 +54,34 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: [
                     Container(
-                      child: Text(
-                        tx.amount.toString(),
-                      ),
                       margin: EdgeInsets.all(5.0),
                       decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey, width: 2.0),
+                        border: Border.all(color: Colors.purple, width: 2.0),
                       ),
                       padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        '\$ ${tx.amount}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                            color: Colors.purple),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tx.title),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 24,
+                              fontFamily: 'roboto'),
+                        ),
                         Text(
                           tx.date.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueGrey),
                         ),
                       ],
                     )
